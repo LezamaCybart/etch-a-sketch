72221748d7e4f7072166f3c7875a7c6fa3459866
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-02-08 10:56:25 -0300

    implementing random colors and darkening functions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 This project is part the The Odin Project curriculum (https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project) 
 
+live demo: https://lezamacybart.github.io/etch-a-sketch/
+
+Functionality: 
+    *It creates a default 16x16 grid.
+    *When the cursor hovers over a cell of the grid, the cell changes to a random color, leaving a (pixelated) trail through your grid like a pen would.
+    *Each pass of the cursor darkens the color of the cell by 10%, turning it black after 10 passes.
+    *The "clear and set size" button clears the grid, and prompts the user for a new grid size-
+
+Thoughts:
+    The project is a really good DOM manipulation using VanillaJS practice exercise. It ended up coming together a lot quicker than I thought it would, the hardest task was darkening the cells with each hover.
--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
 const container = document.getElementById("grid-container");
 
 function paint(e) {
-    e.target.style.backgroundColor = "grey";
+  let cellCollor = e.target.style.backgroundColor;
+
+  if (cellCollor == "") {
+    let randomColor = getRandomColor();
+    e.target.style.backgroundColor = randomColor;
+    e.target.style.webkitFilter = "brightness(1)";
+    e.target.value = 1;
+  } else {
+    let brightnessValue = e.target.value;
+
+    if (brightnessValue > 0) {
+      e.target.value -= 0.1;
+      e.target.style.webkitFilter = `brightness(${e.target.value})`;
+    }
+  }
+}
+
+function getRandomColor() {
+  let randomColor = `rgba(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`
+  return randomColor;
+}
+
+function getRandomNumber() {
+  return Math.random() * 255;
 }
 
 function makeRows(rows, cols) {
 
-//we first reset the existing grid by deleting every cell.
+  //we first reset the existing grid by deleting every cell.
   container.querySelectorAll(".grid-item").forEach(e => e.remove());
 
   container.style.setProperty('--grid-rows', rows);
@@ -18,16 +41,18 @@ function makeRows(rows, cols) {
   };
 };
 
+makeRows(16,16);
+
 //control function, reset and resize grid
 
 const button = document.getElementById("reset-btn");
 
 function resetGrid() {
   let size = prompt("Please enter size between 2 and 64");
-  if ( isNaN(size) ) resetGrid();
-  if ( size < 2 || size > 64 ) resetGrid();
+  if (isNaN(size)) resetGrid();
+  if (size < 2 || size > 64) resetGrid();
 
   makeRows(size, size);
 }
 
-button.addEventListener( "click", resetGrid )
+button.addEventListener("click", resetGrid)
